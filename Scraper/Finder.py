a4a3d2b26c9b64b393327b8e278beefb6680c615
Darius McFarland <indycolts1893@gmail.com>
2018-03-31 10:57:47 -0400

    Added the finder program to a package and began to start the webscraping
    process

--- Scraper/Finder.py
+++ Scraper/Finder.py
@@ -3,6 +3,16 @@
 # http://www.adidas.com/us/ultraboost-shoes/BB6166_610.html?forceSelSize=BB6166_610
 # https://www.adidas.com/us/ultraboost-shoes/BB6167.html
 
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:59.0) Gecko/20100101 Firefox/59.0'}
+
+import bs4
+
+import requests
+
+import random
+
+import webbrowser
+
 
 def create_url(model, size):
     # Set the lowest shoe size to look at to be a Men's size 6.5
@@ -12,7 +22,7 @@ def create_url(model, size):
     shoe_size_code = size - 6.5
 
     # Multiply the size code by 20 because Adidas increases the value for each whole size by 20
-    shoe_size_code = size * 20
+    shoe_size_code = shoe_size_code * 20
 
     # Add this size code to the start size to create a code for any sized shoe
     temp = start_size + shoe_size_code
@@ -25,3 +35,25 @@ def create_url(model, size):
           '_' + str(shoe_size_code)
 
     return url
+
+
+# model = input("Model # ")
+#
+# size = input("Size ")
+#
+# new_url = create_url(model, float(size))
+#
+# print(new_url)
+
+# Base url: black ultraboosts in a size 6.5
+url = 'http://www.adidas.com/us/ultraboost-shoes/BB6166_580.html?forceSelSize=BB6166_580'
+
+print(url)
+# Get the information from the url
+response = requests.get(url, headers=headers)
+
+# Get the webpage and ensure that it's correct by checking the webpage title
+
+webpage = bs4.BeautifulSoup(response.text, "html.parser")
+
+print(webpage.title.string)
